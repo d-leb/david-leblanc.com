f10fb717bbfa3ae711e0856501fa8abab93c2fd8
David LeBlanc <email@david-leblanc.com>
2021-02-27 21:44:30 -0500

    Modify components to be a11y compliant

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,9 @@ import React from 'react'
 
 import { Placeholder } from 'components/Placeholder'
 
-export const App = () => <Placeholder />
+export const App = () => (
+  <main>
+    <h1>Placeholder Heading</h1>
+    <Placeholder />
+  </main>
+)
