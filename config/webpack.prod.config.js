de1494d8dc65ce2fdfb0c5b2b26eeb89d41ea0ba
David LeBlanc <email@david-leblanc.com>
2021-02-28 21:21:11 -0500

    Add react-intl provider with starting translation files

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -15,5 +15,6 @@ module.exports = {
   },
   performance: {
     hints: 'error',
+    maxEntrypointSize: 300000,
   },
 }
--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,10 +1,20 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
+import { IntlProvider } from 'react-intl'
 import { BrowserRouter } from 'react-router-dom'
+
+import { messages } from 'translations'
 
 interface Props {
   children: ReactNode
 }
 
-const Providers = ({ children }: Props) => <BrowserRouter>{children}</BrowserRouter>
+const Providers = ({ children }: Props) => {
+  const [locale] = useState(navigator.language.split(/[-_]/)[0])
+  return (
+    <IntlProvider {...{ locale }} messages={messages[locale]} defaultLocale="en">
+      <BrowserRouter>{children}</BrowserRouter>
+    </IntlProvider>
+  )
+}
 
 export default Providers
