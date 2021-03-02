ccb760687dc878416b58170d09ddde13420c4aff
David LeBlanc <email@david-leblanc.com>
2021-03-02 08:26:03 -0500

    Add accessibility testing to jest tests

--- src/components/Placeholder/Placeholder.test.tsx
+++ src/components/Placeholder/Placeholder.test.tsx
@@ -1,7 +1,10 @@
+import { axe, toHaveNoViolations } from 'jest-axe'
 import React from 'react'
 
 import { render } from 'devtools/testing-library'
 import { Placeholder } from './index'
+
+expect.extend(toHaveNoViolations)
 
 describe('Placeholder', () => {
   it('renders without error', () => {
@@ -9,5 +12,12 @@ describe('Placeholder', () => {
     const placeholderText = getByText(/placeholder/i)
 
     expect(placeholderText).toBeDefined()
+  })
+
+  it('adheres to accessibility standards', async () => {
+    const { container } = render(<Placeholder />)
+    const results = await axe(container)
+
+    expect(results).toHaveNoViolations()
   })
 })
