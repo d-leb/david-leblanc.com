3e888ce9827dba40ce756c4cccc78d8268098643
David LeBlanc <email@david-leblanc.com>
2021-03-07 14:12:34 -0500

    Add Open Sans font support

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,14 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
 import styledNormalize from 'styled-normalize'
 
+import { Colors } from 'styles/Colors'
+
 export const GlobalStyle = createGlobalStyle`
   ${styledNormalize}
 
   body {
     margin: 0;
     padding: 0;
-    font-family: Arial, sans-serif;
-    font-weight: 300;
-    font-size: 1em
+    background-color: ${Colors.IVORY};
+
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Colors.JET};
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Open Sans', sans-serif;
+      font-weight: 600;
+    }
+    h1 {
+      font-size: 3em;
+    }
+    h2 {
+      font-size: 2em;
+    }
+    h3 {
+      font-size: 1.4em;
+    }
   }
 `
