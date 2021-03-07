e416672fd02ff2be890f56080ef820c3a12974c6
David LeBlanc <email@david-leblanc.com>
2021-03-07 18:31:46 -0500

    Create Heading component with tests and storybook

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { Colors } from 'styles/Colors'
+import { messages } from 'translations'
+import { Heading as HeadingComponent } from './index'
+
+export default {
+  title: 'Design System/Components/Heading',
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: [...Object.keys(Colors)],
+      },
+    },
+    level: {
+      control: {
+        type: 'range',
+        min: 1,
+        max: 6,
+        step: 1,
+      },
+    },
+    messageId: {
+      control: {
+        type: 'select',
+        options: [...Object.keys(messages.en)],
+      },
+    },
+    messageValues: {
+      control: {
+        type: 'object',
+      },
+    },
+  },
+}
+
+type Props = Parameters<typeof HeadingComponent>[0]
+export const Heading = (props: Props) => <HeadingComponent {...props} />
+Heading.args = {
+  color: undefined,
+  level: 2,
+  messageId: 'lorem-ipsum.sentence-01',
+  messageValues: undefined,
+}
